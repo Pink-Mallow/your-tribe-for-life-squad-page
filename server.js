3cf6cd9da3ff18fc45d378a8efbf07062497e99c
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2026-09-04 16:14:20 +0200

    feat: data fetching for all persons and extra
    This commit concerns issue #5, #6
    Data has been fetched for all the persons (and extra) on the review and detail section.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-export async function load(){
-  const res = await fetch(`https://fdnd.directus.app/items/squad`);
-  const data = await res.json;
-  console.log(data);
+// export async function load(){
+//   const res = await fetch(`https://fdnd.directus.app/items/squad`);
+//   const data = await res.json;
+//   console.log(data);
 
-  return {
-    persons: data.data
-  }
-}
+//   return {
+//     persons: data.data
+//   }
+// }
