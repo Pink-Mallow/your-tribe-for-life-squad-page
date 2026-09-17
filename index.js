be53348beb074146415d166284b461df3875c274
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2026-09-17 12:16:01 +0200

    feature: added global.css, assets in public, serve public map to browser
    This commit concerns issue #4, #19 #16.
    
    Moved assets into public map.
    Added custom properties with multiple colour values in the `global.css`.
    Added link to stylesheet in the` head.liquid`.
    Added `express.static` for the `public map` to serve static files to the browser.

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const apiUrl = "https://fdnd.directus.app/items/person?*.*.*&filter[squads][squa
 // persons https://fdnd.directus.app/items/person?fields=&filter[squads][squad_id][cohort][_eq]=2627
 // name and id https://fdnd.directus.app/items/person?fields=name,id&filter[squads][squad_id][cohort][_eq]=2627
 // 5 fields https://fdnd.directus.app/items/person?*.*.*&filter[squads][squad_id][cohort][_eq]=2627&fields=name,fav_season,bio,profilecard,fav_animal
+
 app.use(express.urlencoded({ extended: true }));
+
+// The app.use(express.static('assets')) code is used to serve static files such as 
+// images, CSS files, and JavaScript files in an Express app. 
+// The express.static middleware function is used to expose a directory or a file to a 
+// particular URL so its contents can be publicly accessed. 
+app.use(express.static('public'));
 
 app.engine('liquid', engine.express());
 app.set('views', './views');
